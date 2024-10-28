0d3a7460d0bc6ab91cf342f780f35616f4e31d0a
rpaternina <robertpaternina65@gmail.com>
2024-10-28 13:51:35 -0500

    Microservicio catalogo listo

---
+++ userServices/src/main/java/com/user/user_Services/UserServicesApplication.java
@@ -0,0 +1,13 @@
+package com.user.user_Services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServicesApplication.class, args);
+	}
+
+}
---
+++ userServices/src/test/java/com/user/user_Services/UserServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.user.user_Services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
